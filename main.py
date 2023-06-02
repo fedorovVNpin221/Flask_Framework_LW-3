e8db90114c5b6ccfc5ae6136d9aa444505e52676
tolya223 <125587751+fedorovVNpin221@users.noreply.github.com>
2023-06-02 12:26:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ def index():
     return render_template("index.html")
 
 
+@site.route('/calculate', methods=['POST'])
+def calculate(a, b, c):
+    discriminant = b ** 2 - 4 * a * c
+
+    if discriminant > 0:
+        x1 = (-b + (math.sqrt(discriminant))) / (2 * a)
+        x2 = (-b - (math.sqrt(discriminant))) / (2 * a)
+        result = "Корни уравнения: x1 = {}, x2 = {}".format(x1, x2)
+    elif discriminant == 0:
+        x = -b / (2 * a)
+        result = "Уравнение имеет единственный корень: x = {}".format(x)
+    else:
+        result = "Уравнение не имеет действительных корней"
+
+    return render_template('index.html', result=result)
+
 if __name__ == "__main__":
     site.run(debug=True)
 
